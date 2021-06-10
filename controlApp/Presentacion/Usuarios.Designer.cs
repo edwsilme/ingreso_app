a15e2497d761f21c41581a8c1d4c738602ef67f0
edwsilme <eesilva2@gmail.com>
2021-06-10 15:52:16 -0500

    Add C# code

---
+++ controlApp/Presentacion/Usuarios.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace controlApp.Presentacion
+{
+    partial class Usuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Usuarios));
+            this.pTitulo = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.dataListado = new System.Windows.Forms.DataGridView();
+            this.panelUsuario = new System.Windows.Forms.Panel();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.btnGuardarC = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.lbIcono = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lbPass = new System.Windows.Forms.Label();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.dlg = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.imgIcono = new System.Windows.Forms.PictureBox();
+            this.ImgBuscador = new System.Windows.Forms.PictureBox();
+            this.Eliminar = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Editar = new System.Windows.Forms.DataGridViewImageColumn();
+            this.pTitulo.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataListado)).BeginInit();
+            this.panelUsuario.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcono)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgBuscador)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pTitulo
+            // 
+            this.pTitulo.Controls.Add(this.panel1);
+            this.pTitulo.Controls.Add(this.lblTitulo);
+            this.pTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pTitulo.Name = "pTitulo";
+            this.pTitulo.Size = new System.Drawing.Size(771, 69);
+            this.pTitulo.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnAgregar);
+            this.panel1.Controls.Add(this.ImgBuscador);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.txtBuscar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(771, 38);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnAgregar.Font = new System.Drawing.Font("Sigmar One", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(269, 6);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(93, 29);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(6, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(190, 1);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBuscar.Location = new System.Drawing.Point(6, 12);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(190, 13);
+            this.txtBuscar.TabIndex = 0;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(771, 31);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Usuarios";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dataListado
+            // 
+            this.dataListado.AllowUserToDeleteRows = false;
+            this.dataListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataListado.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Eliminar,
+            this.Editar});
+            this.dataListado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataListado.Location = new System.Drawing.Point(0, 69);
+            this.dataListado.Name = "dataListado";
+            this.dataListado.Size = new System.Drawing.Size(771, 381);
+            this.dataListado.TabIndex = 1;
+            this.dataListado.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataListado_CellContentClick);
+            // 
+            // panelUsuario
+            // 
+            this.panelUsuario.Controls.Add(this.btnVolver);
+            this.panelUsuario.Controls.Add(this.btnGuardarC);
+            this.panelUsuario.Controls.Add(this.btnGuardar);
+            this.panelUsuario.Controls.Add(this.imgIcono);
+            this.panelUsuario.Controls.Add(this.lbIcono);
+            this.panelUsuario.Controls.Add(this.txtPassword);
+            this.panelUsuario.Controls.Add(this.txtUsuario);
+            this.panelUsuario.Controls.Add(this.lbPass);
+            this.panelUsuario.Controls.Add(this.lbNombre);
+            this.panelUsuario.Controls.Add(this.lblUsuario);
+            this.panelUsuario.Location = new System.Drawing.Point(245, 89);
+            this.panelUsuario.Name = "panelUsuario";
+            this.panelUsuario.Size = new System.Drawing.Size(496, 323);
+            this.panelUsuario.TabIndex = 2;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnVolver.Font = new System.Drawing.Font("Sigmar One", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.Location = new System.Drawing.Point(356, 265);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(93, 29);
+            this.btnVolver.TabIndex = 9;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // btnGuardarC
+            // 
+            this.btnGuardarC.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnGuardarC.Font = new System.Drawing.Font("Sigmar One", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarC.Location = new System.Drawing.Point(157, 265);
+            this.btnGuardarC.Name = "btnGuardarC";
+            this.btnGuardarC.Size = new System.Drawing.Size(179, 29);
+            this.btnGuardarC.TabIndex = 8;
+            this.btnGuardarC.Text = "Guardar Cambios";
+            this.btnGuardarC.UseVisualStyleBackColor = false;
+            this.btnGuardarC.Click += new System.EventHandler(this.btnGuardarC_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnGuardar.Font = new System.Drawing.Font("Sigmar One", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(45, 265);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(93, 29);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // lbIcono
+            // 
+            this.lbIcono.AutoSize = true;
+            this.lbIcono.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIcono.ForeColor = System.Drawing.Color.Black;
+            this.lbIcono.Location = new System.Drawing.Point(86, 133);
+            this.lbIcono.Name = "lbIcono";
+            this.lbIcono.Size = new System.Drawing.Size(52, 20);
+            this.lbIcono.TabIndex = 5;
+            this.lbIcono.Text = "Icono";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(161, 85);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(189, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(161, 44);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(189, 20);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // lbPass
+            // 
+            this.lbPass.AutoSize = true;
+            this.lbPass.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPass.ForeColor = System.Drawing.Color.Black;
+            this.lbPass.Location = new System.Drawing.Point(42, 89);
+            this.lbPass.Name = "lbPass";
+            this.lbPass.Size = new System.Drawing.Size(96, 20);
+            this.lbPass.TabIndex = 2;
+            this.lbPass.Text = "Contraseña";
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombre.ForeColor = System.Drawing.Color.Black;
+            this.lbNombre.Location = new System.Drawing.Point(71, 44);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(67, 20);
+            this.lbNombre.TabIndex = 1;
+            this.lbNombre.Text = "Usuario";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Location = new System.Drawing.Point(120, 31);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(35, 13);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "label1";
+            // 
+            // dlg
+            // 
+            this.dlg.FileName = "openFileDialog1";
+            // 
+            // dataGridViewImageColumn1
+            // 
+            this.dataGridViewImageColumn1.HeaderText = "Eliminar";
+            this.dataGridViewImageColumn1.Image = global::controlApp.Properties.Resources.boton_x;
+            this.dataGridViewImageColumn1.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
+            // 
+            // imgIcono
+            // 
+            this.imgIcono.Image = global::controlApp.Properties.Resources.camara;
+            this.imgIcono.Location = new System.Drawing.Point(161, 133);
+            this.imgIcono.Name = "imgIcono";
+            this.imgIcono.Size = new System.Drawing.Size(100, 79);
+            this.imgIcono.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgIcono.TabIndex = 6;
+            this.imgIcono.TabStop = false;
+            this.imgIcono.Click += new System.EventHandler(this.imgIcono_Click);
+            // 
+            // ImgBuscador
+            // 
+            this.ImgBuscador.Image = global::controlApp.Properties.Resources.buscar1;
+            this.ImgBuscador.InitialImage = global::controlApp.Properties.Resources.buscar;
+            this.ImgBuscador.Location = new System.Drawing.Point(210, 7);
+            this.ImgBuscador.Name = "ImgBuscador";
+            this.ImgBuscador.Size = new System.Drawing.Size(29, 24);
+            this.ImgBuscador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ImgBuscador.TabIndex = 2;
+            this.ImgBuscador.TabStop = false;
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.HeaderText = "Eliminar";
+            this.Eliminar.Image = global::controlApp.Properties.Resources.boton_x;
+            this.Eliminar.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Eliminar.Name = "Eliminar";
+            // 
+            // Editar
+            // 
+            this.Editar.HeaderText = "Editar";
+            this.Editar.Image = global::controlApp.Properties.Resources.editar;
+            this.Editar.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Editar.Name = "Editar";
+            // 
+            // Usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 450);
+            this.Controls.Add(this.panelUsuario);
+            this.Controls.Add(this.dataListado);
+            this.Controls.Add(this.pTitulo);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Usuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UsuariosProyecto";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pTitulo.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataListado)).EndInit();
+            this.panelUsuario.ResumeLayout(false);
+            this.panelUsuario.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcono)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgBuscador)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pTitulo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.PictureBox ImgBuscador;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dataListado;
+        private System.Windows.Forms.Panel panelUsuario;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.PictureBox imgIcono;
+        private System.Windows.Forms.Label lbIcono;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lbPass;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnGuardarC;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.OpenFileDialog dlg;
+        private System.Windows.Forms.DataGridViewImageColumn Eliminar;
+        private System.Windows.Forms.DataGridViewImageColumn Editar;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn1;
+    }
+}
